ac08dcbcc3318393356666b1a277a6c65b2f2759
Selt0 <chewedgum12@gmail.com>
2020-07-05 14:05:53 -0500

    Create new default values to test indices

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -7,7 +7,7 @@
 #
 class Dog < ApplicationRecord
    validates :name, presence: true
-   validate :check_name_length 
+  #  validate :check_name_length 
 
   belongs_to :house,
     primary_key: :id,
@@ -20,8 +20,14 @@ class Dog < ApplicationRecord
     class_name: :Toy
 
    def check_name_length
-    unless self.name.length >= 4
-      errors[:name] << "is too short, must be at least 4 characters."
+    unless self.name.length >= 3
+      errors[:name] << "is too short, must be at least 3 characters."
     end
+   end
+
+   def self.lookup_name_in_ms(name)
+    start = Time.now
+    Dog.where(name: name)
+    (Time.now - start) * 1000
    end
 end
